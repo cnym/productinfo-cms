38adf9817fa16c3ab3b719d0fc206f34a51dc8cb
yanjinhui <18323348262@139.com>
2019-08-27 14:32:23 +0800

    测试提交错误文件

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -17,7 +17,7 @@
   import HeaderNav from './components/headerNav'
   import SideBar from './components/sideBar'
   export default {
-    name: 'cms1',
+    name: 'cms',
     components: {
       HeaderNav,
       SideBar
